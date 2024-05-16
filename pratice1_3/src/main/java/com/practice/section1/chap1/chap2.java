f152cb740602a189f7e30521b1a197543b272d98
HEun0420 <usialeta@gmail.com>
2024-05-16 18:10:19 +0900

    연습

---
+++ pratice1_3/src/main/java/com/practice/section1/chap1/chap2.java
@@ -0,0 +1,27 @@
+package com.practice.section1.chap1;
+
+public class chap2 {
+    public static void main(String[] args) {
+
+        // 자바의 정석
+        /* 2 챕터. 변수- 연습문제*/
+
+        //2-1
+        /* 정수형
+        * byte(1 byte), short(2 byte), int(4 byte), long(8 byte)
+        * 실수형
+        * float(4 byte), double(8 byte)
+        * 문자형
+        * char(2 byte)
+        * 논리형
+        * boolean(1 byte)
+        * 참조형
+        * star
+         */
+
+        //2-2
+
+
+
+    }
+}
